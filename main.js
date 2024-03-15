52bca18756617002971b14f7ff16491d3eb8601e
Usama Muzammil <160235187+UsamaIT786@users.noreply.github.com>
2024-03-15 18:12:32 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+/*
+Store your favorite number in a variable. Then, using that variable, create a message that
+reveals your favorite number. Print that message.
+*/
+var favouritenumber = 20;
+console.log("My favourite number is ".concat(favouritenumber));
---
+++ main.ts
@@ -0,0 +1,7 @@
+/*
+Store your favorite number in a variable. Then, using that variable, create a message that
+reveals your favorite number. Print that message.
+*/
+
+let favouritenumber: number= 20;
+console.log(`My favourite number is ${favouritenumber}`);
